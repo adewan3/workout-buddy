64efdc963dd667d1ffff3ee7b2e982f94a59a983
adewan3 <adewan3@asu.edu>
2024-05-31 11:26:21 -0700

    Added delete feature in the workouts api

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -11,10 +11,10 @@ route.get('/',async (req,res)=>{
 })
 
 //GET a single workout
-route.get('/:id',(req,res)=>{
-    const id = req.params;
+// route.get('/:id',(req,res)=>{
+//     const id = req.params;
     
-})
+// })
 
 //POST a workout
 route.post('/',async (req,res)=>{
@@ -45,7 +45,7 @@ route.post('/',async (req,res)=>{
 //PUT request
 route.put('/:id',async (req,res)=>{
     const {title,reps, load} = req.body;
-    const id = req.params;
+    const id = req.params.id;
     try{
         const data = await workoutSchema.findById(id);
         if(!data){
@@ -64,6 +64,29 @@ route.put('/:id',async (req,res)=>{
     }
     
     
+    
+})
+
+//DELETE a workout
+route.delete('/:id',async (req,res)=>{
+    
+    try{
+
+        const id = req.params.id;
+    
+        const data = await workoutSchema.findById(id);
+        if(!data){
+            res.status(400).send({msg:"No such Id present"});
+        }
+        await workoutSchema.findByIdAndDelete(id);
+        res.status(200).send({ msg: "Record deleted successfully" });
+
+    }catch(error){
+        res.status(400).send({msg:error.message});
+
+    }
+    
+
     
 })
 
