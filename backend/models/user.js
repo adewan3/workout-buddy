ccb5e1e05a099c3eb3b7bc6a157a8c0d690b49b5
adewan3 <adewan3@asu.edu>
2024-06-18 17:50:47 -0500

    validator added in user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,8 @@
 const mongoose  = require('mongoose');
 const bcrypt = require('bcrypt');
+const validator = require('validator');
+
+
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -16,10 +19,24 @@ const userSchema = new Schema({
 })
 
 userSchema.statics.signup = async function(email, password){
+
+    if(!email && !password){
+        throw new Error("All the fields are empty");
+    }
+    if(!validator.isEmail(email)){
+
+        throw new Error("All the fields are empty");
+
+    }
+    if(!validator.isStrongPassword(password)){
+
+        throw new Error("Password is not strong");
+    }
+
     const useremail = await this.findOne({email});
     console.log(useremail);
     if(useremail){
-        throw new Error("Username already present");
+        throw new Error("email already registered");
     }
     const salt =  await bcrypt.genSalt(10);
     const hashpassword = await bcrypt.hash(password,salt);
